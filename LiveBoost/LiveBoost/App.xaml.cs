7308fa15291dc2cd118bf22b52a226ca63dbab71
lizhengbo <lizhengbo_0304@163.com>
2023-09-04 10:43:03 +0800

    1111

---
+++ LiveBoost/LiveBoost/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace LiveBoost
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App { }
+}
---
+++ LiveBoost/LiveBoost/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LiveBoost
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
